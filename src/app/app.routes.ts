b2a0b5fad8256309b4bddf446fc978f8a767ff88
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-06-20 00:18:38 +1000

    [src/app/app.routes.ts,tsconfig.json] Fix compilation (Angular 11?)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  {path: '', loadChildren: () => import('app/slash/slash.module').then(m => m.SlashModule)},
-  {path: 'docs/getting-started', loadChildren: () => import('app/getting-started/getting-started.module').then(m => m.GettingStartedModule)},
-  {path: 'docs/learn-engineering', loadChildren: () => import('app/learn-engineering/learn-engineering.module').then(m => m.LearnEngineeringModule)},
-  {path: 'research/open-questions', loadChildren: () => import('app/open-questions/open-questions.module').then(m => m.OpenQuestionsModule)}
+  {path: '',
+    loadChildren: () => import('./slash/slash.module').then(m => m.SlashModule)},
+  {path: 'docs/getting-started',
+    loadChildren: () => import('./getting-started/getting-started.module').then(m => m.GettingStartedModule)},
+  {path: 'docs/learn-engineering',
+    loadChildren: () => import('./learn-engineering/learn-engineering.module').then(m => m.LearnEngineeringModule)},
+  {path: 'research/open-questions',
+    loadChildren: () => import('./open-questions/open-questions.module').then(m => m.OpenQuestionsModule)}
 ];
