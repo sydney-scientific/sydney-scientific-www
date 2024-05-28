546bfe8a3d68b7740b19a64730371366ee123dce
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-05-28 00:01:03 -0400

    [*] Angular 18 ; [.github/workflows/main.yml] Fix Angular 18 with Vite build system; parameterise whole task for better reuse across other sites

--- src/app/svg-viewer/svg-viewer.module.ts
+++ src/app/svg-viewer/svg-viewer.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { SvgViewerComponent } from './svg-viewer.component';
 
-@NgModule({
-  exports: [SvgViewerComponent],
-  imports: [
-    CommonModule, HttpClientModule,
-    FlexLayoutModule
-  ],
-  declarations: [SvgViewerComponent],
-})
+@NgModule({ exports: [SvgViewerComponent],
+    declarations: [SvgViewerComponent], imports: [CommonModule,
+        FlexLayoutModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SvgViewerModule {
 }
